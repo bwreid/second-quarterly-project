063d543dc6125cc98d4d516dccef60375ae49d0a
Bryan W. Reid <bwreid@gmail.com>
2013-03-12 18:26:20 -0400

    heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require 'foundation/js/foundation.min.js'
 //= require 'foundation/js/vendor/custom.modernizr.js'
+//= require 'foundation/js/foundation.min.js'
 //= require 'foundation/js/vendor/zepto.js'
 //= require 'morris/spec/vendor/raphael-2.1.0.min.js'
 //= require 'morris/morris.min.js'
