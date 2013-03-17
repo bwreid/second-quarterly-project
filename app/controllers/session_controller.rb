7ea92f543f2f9c497a8b53eb1f044c1543e1cfac
Bryan W. Reid <bwreid@gmail.com>
2013-03-17 09:11:14 -0400

    removing username

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -5,7 +5,7 @@ class SessionController < ApplicationController
   end
 
   def create
-    user = User.where( :username => params[:username] ).first();
+    user = User.where( :email => params[:email] ).first();
 
     if user.present? && user.authenticate( params[:password] )
       session[:user_id] = user.id
